5d6478548f2d4adff0be84f2415ac39944ac1221
Mijin Sim <202110106@bssm.hs.kr>
2023-10-11 11:21:27 +0900

    TEST(font) : font적용 테스트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Title } from "./styles/font";
+import { Body, ButtonText, CustomFont, Title } from "./styles/font";
 
 const App = () => {
   return (
     <main>
       <Title>Hello world</Title>
+      <br/>
+      <Body>Hi</Body>
+      <br/>
+      <CustomFont>This is CustomFont</CustomFont>
+      <br/>
+      <ButtonText>This is ButtonText</ButtonText>
     </main>
   );
 }
