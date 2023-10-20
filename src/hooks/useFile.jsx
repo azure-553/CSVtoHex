44c7c6e8712391e31e0b0b1af8f3d01a550db559
Mijin Sim <202110106@bssm.hs.kr>
2023-10-20 10:29:26 +0900

    CHORE : setTimeOut 시간 조절

--- src/hooks/useFile.jsx
+++ src/hooks/useFile.jsx
@@ -47,7 +47,7 @@ export default function useFile() {
     setTimeout(() => {
       setIsChanged(false)
       setIsLoading(false)
-    }, 3000)
+    }, 2000)
   }
 
   const handleFileDownload = async () => {
