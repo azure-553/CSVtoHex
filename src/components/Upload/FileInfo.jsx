f7060c789744612b009f0b635af823307f759ab0
Mijin Sim <202110106@bssm.hs.kr>
2023-10-20 17:07:52 +0900

    CHORE : 구분자 공백으로 변경

--- src/components/Upload/FileInfo.jsx
+++ src/components/Upload/FileInfo.jsx
@@ -4,33 +4,20 @@ import { Title } from '../../styles/font'
 import csvToJSON from '../../utils/csvToJson'
 
 export const FileInfo = ({ uploadedInfo }) => {
-
   const stringUploadInfo = String(uploadedInfo)
   let csvContent = csvToJSON(stringUploadInfo)
 
   return (
     <InfoBox>
-        {csvContent.map((item) => (
-          <>
-            <Title size="2.5rem">
-              {item.id} |
-              {item.HEAT_METER} |
-              {item.c1} |
-              {item.c2} |
-              {item.c3} |
-              {item.c4} |
-              {item.c5} |
-              {item.c6} |
-              {item.a1} |
-              {item.c7} |
-              {item.a2} |
-              {item.c8} |
-              {item.c9} |
-              {item.c10} |
-              <br />
-            </Title>
-          </>
-        ))}
+      {csvContent.map((item) => (
+        <>
+          <Title size="2.5rem">
+            {item.id} {item.HEAT_METER} {item.c1} {item.c2} {item.c3} {item.c4}{' '}
+            {item.c5} {item.c6} {item.a1} {item.c7} {item.a2} {item.c8}{' '}
+            {item.c9} {item.c10} <br />
+          </Title>
+        </>
+      ))}
     </InfoBox>
   )
 }
