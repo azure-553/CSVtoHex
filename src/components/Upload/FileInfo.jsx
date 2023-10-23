ca917665835a5aac9e60079e431c022352609f1a
Mijin Sim <202110106@bssm.hs.kr>
2023-10-23 17:18:08 +0900

    FEAT(csvContent) : 아스키코드로 변환한 값들 콘솔로 찍기

--- src/components/Upload/FileInfo.jsx
+++ src/components/Upload/FileInfo.jsx
@@ -6,7 +6,79 @@ import csvToJSON from '../../utils/csvToJson'
 export const FileInfo = ({ uploadedInfo }) => {
   const stringUploadInfo = String(uploadedInfo)
   let csvContent = csvToJSON(stringUploadInfo)
-  console.log(csvContent);
+  csvContent.map((item) => {
+    const seq = (item.seq || '').split('')
+    const deviceId = (item.device_id || '').split('')
+    const tagCode = (item.tag_code || '').split('')
+    const reqSet = (item.req_set || '').split('')
+    const func = (item.func || '').split('')
+    const unitId = (item.unit_id || '').split('')
+    const reserved = (item.Reserved || '').split('')
+    const address = (item.address || '').split('')
+    const endian = (item.endian || '').split('')
+    const wordcnt = (item.wordcnt || '').split('')
+    const format = (item.format || '').split('')
+    const scale = (item.scale || '').split('')
+    const useFlag = (item.Use_flag || '').split('')
+    const port = (item.Port || '').split('')
+
+    seq.forEach((element) => {
+      const seqHex = element.charCodeAt()
+      console.log(seqHex)
+    })
+    deviceId.forEach((element) => {
+      const deviceIdHex = element.charCodeAt()
+      console.log(deviceIdHex)
+    })
+    tagCode.forEach((element) => {
+      const tagCodeHex = element.charCodeAt()
+      console.log(tagCodeHex)
+    })
+    reqSet.forEach((element) => {
+      const reqSetHex = element.charCodeAt()
+      console.log(reqSetHex)
+    })
+    func.forEach((element) => {
+      const funcHex = element.charCodeAt()
+      console.log(funcHex)
+    })
+    unitId.forEach((element) => {
+      const unitIdHex = element.charCodeAt()
+      console.log(unitIdHex)
+    })
+    reserved.forEach((element) => {
+      const reservedHex = element.charCodeAt()
+      console.log(reservedHex)
+    })
+    address.forEach((element) => {
+      const addressHex = element.charCodeAt()
+      console.log(addressHex)
+    })
+    endian.forEach((element) => {
+      const endianHex = element.charCodeAt()
+      console.log(endianHex)
+    })
+    wordcnt.forEach((element) => {
+      const wordcntHex = element.charCodeAt()
+      console.log(wordcntHex)
+    })
+    format.forEach((element) => {
+      const formatHex = element.charCodeAt()
+      console.log(formatHex)
+    })
+    scale.forEach((element) => {
+      const scaleHex = element.charCodeAt()
+      console.log(scaleHex)
+    })
+    useFlag.forEach((element) => {
+      const useFlagHex = element.charCodeAt()
+      console.log(useFlagHex)
+    })
+    port.forEach((element) => {
+      const portHex = element.charCodeAt()
+      console.log(portHex)
+    })
+  })
 
   return (
     <InfoBox>
