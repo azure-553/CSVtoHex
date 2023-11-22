4924d87e82ef98ffb4e39c2bf1ae611b674e937a
Mijin Sim <202110106@bssm.hs.kr>
2023-11-22 09:44:21 +0900

    chore : format에 0 채워주기

--- src/utils/formatValue.js
+++ src/utils/formatValue.js
@@ -7,39 +7,48 @@ export default function formatValue(arrCsvContentHex, format) {
   switch (format) {
     case formats.formatJ:
       formatHex = 'J'
-      arrCsvContentHex.push(formatHex.charCodeAt() + formatHexFixValue)
+      arrCsvContentHex.push(formatHex.charCodeAt())
+      arrCsvContentHex.push(formatHexFixValue)
       break
     case formats.formatI:
       formatHex = 'I'
-      arrCsvContentHex.push(formatHex.charCodeAt() + formatHexFixValue)
+      arrCsvContentHex.push(formatHex.charCodeAt())
+      arrCsvContentHex.push(formatHexFixValue)
       break
     case formats.formatK:
       formatHex = 'K'
-      arrCsvContentHex.push(formatHex.charCodeAt() + formatHexFixValue)
+      arrCsvContentHex.push(formatHex.charCodeAt())
+      arrCsvContentHex.push(formatHexFixValue)
       break
     case formats.formatU:
       formatHex = 'U'
-      arrCsvContentHex.push(formatHex.charCodeAt() + formatHexFixValue)
+      arrCsvContentHex.push(formatHex.charCodeAt())
+      arrCsvContentHex.push(formatHexFixValue)
       break
     case formats.formatV:
       formatHex = 'V'
-      arrCsvContentHex.push(formatHex.charCodeAt() + formatHexFixValue)
+      arrCsvContentHex.push(formatHex.charCodeAt())
+      arrCsvContentHex.push(formatHexFixValue)
       break
     case formats.formatW:
       formatHex = 'W'
-      arrCsvContentHex.push(formatHex.charCodeAt() + formatHexFixValue)
+      arrCsvContentHex.push(formatHex.charCodeAt())
+      arrCsvContentHex.push(formatHexFixValue)
       break
     case formats.formatF:
       formatHex = 'F'
-      arrCsvContentHex.push(formatHex.charCodeAt() + formatHexFixValue)
+      arrCsvContentHex.push(formatHex.charCodeAt())
+      arrCsvContentHex.push(formatHexFixValue)
       break
     case formats.formatD:
       formatHex = 'D'
-      arrCsvContentHex.push(formatHex.charCodeAt() + formatHexFixValue)
+      arrCsvContentHex.push(formatHex.charCodeAt())
+      arrCsvContentHex.push(formatHexFixValue)
       break
     case formats.formatB:
       formatHex = 'B'
-      arrCsvContentHex.push(formatHex.charCodeAt() + formatHexFixValue)
+      arrCsvContentHex.push(formatHex.charCodeAt())
+      arrCsvContentHex.push(formatHexFixValue)
       break
   }
 
