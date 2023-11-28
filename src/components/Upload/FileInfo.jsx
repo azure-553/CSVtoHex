1374173b8d0fec1203cc2f45503e2e2c2e8ee9e4
Mijin Sim <202110106@bssm.hs.kr>
2023-11-28 09:48:15 +0900

    chore : Fragment 축약되어있는 부분 명시적으로 작성 후 key prop추가

--- src/components/Upload/FileInfo.jsx
+++ src/components/Upload/FileInfo.jsx
@@ -10,14 +10,14 @@ export const FileInfo = ({ uploadedInfo }) => {
   return (
     <InfoBox>
       {csvContent.map((item) => (
-        <>
+        <React.Fragment key={item.seq}>
           <Title size="2.5rem">
             {item.seq} {item.device_id} {item.tag_code} {item.req_set}{' '}
             {item.func} {item.unit_id} {item.Reserved} {item.address}{' '}
             {item.endian} {item.wordcnt} {item.format} {item.scale}{' '}
             {item.Use_flag} {item.Port} <br />
           </Title>
-        </>
+        </React.Fragment>
       ))}
     </InfoBox>
   )
